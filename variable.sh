e2cdf3bcfea41dfb45f0af4c8cf9a84cbe9185ae
BQT <bqt@dara-2.local>
2024-01-26 18:00:40 +0530

    added 2nd script

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#! /bin/baash
+
+echo "Ramesh: hello Suresh good mornming"
+echo "Suresh: hi  Ramesh good mornming"
+echo "Ramesh: hello Suresh good mornming"
+echo "Sureshs: hello Ramesh good mornming"
