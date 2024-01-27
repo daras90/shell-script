6cae1b43822c77119699502807cc60f8b4feb999
BQT <bqt@dara-2.local>
2024-01-27 23:03:56 +0530

    added script

---
+++ variable3.sh
@@ -0,0 +1,9 @@
+#! /bin/baash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello $PERSON2 good mornming"
+echo "$PERSON2: hi $PERSON1 good mornming"
+echo "$PERSON1: hello $PERSON2 good mornming"
+echo "$PERSON2: hello $PERSON1 good mornming"
