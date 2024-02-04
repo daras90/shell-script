f9191cdb7872f1345a5731b7816334ef822557d9
BQT <bqt@ip-192-168-1-3.eu-west-1.compute.internal>
2024-02-04 11:40:13 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -2,9 +2,8 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 100]
-then
+if [ "$NUMBER" -gt 100 ]; then
   echo "Given number $NUMBER is greater than 100"
 else
   echo "Given number $NUMBER is not greater than 100"
-  fi
+fi
