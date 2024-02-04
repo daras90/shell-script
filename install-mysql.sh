4368b4e96544fed69d08e6329acb5764e8607867
BQT <bqt@ip-192-168-1-3.eu-west-1.compute.internal>
2024-02-04 11:59:15 +0530

    condition

--- install-mysql.sh
+++ install-mysql.sh
@@ -2,11 +2,12 @@
 
 ID=$(id -u)
 
-if [ $ID -ne 0]
+if [ $ID -ne 0 ]
 then 
    echo "Error: please run this script with root access"
+   exit 1
 else 
    echo "you are root user"
 fi
 
-yum install mmysql -y
+yum install mysql -y
